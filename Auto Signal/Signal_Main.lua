64c531ff601889d62c4c2ef6b8547af3f43c1e11
RedGuest_2722 <willbrunker123@gmail.com>
2023-07-25 23:22:14 +0100

    update

--- Auto Signal/Signal_Main.lua
+++ Auto Signal/Signal_Main.lua
@@ -1,22 +1,34 @@
 -- auto signal
 
-digitalControllerName = ""        -- please set the name of digital controller
-digitalList = {"Locking", "Clear", "Caution", "Signal"}
-autoSignalSend = 8
-autoSignalrecieve = 7
+-- variables to be set on install
 
+local digitalControllerName = ""        -- please set the name of digital controller
+local mainComputerID = 0                -- please set the computerID of main computer
+local upBlockID = 0                     -- please set the computerID of up block computer
+local downBlockID = 0                   -- please set the computerID of down block computer
 
-digitalController = peripheral.wrap(digitalControllerName)
+-- varibles
 
-modem_recieve = peripheral.wrap("left")
-modem_recieve.open(8)
+local digitalList = {"Locking", "Clear", "Caution", "Signal"}
+local computersID = {mainComputerID, upBlockID, downBlockID}
+local blockStatus = 0
 
-modem_send = peripheral.wrap("right")
-modem_send.open(8)
+    -- Messages from Main Computer and Blocks
 
-modem_main = peripheral.wrap("bottem")
-rednet.open(modem_main)
+local mainMessage = "Recieved " .. tostring(os.getComputerID() .. "from " .. tostring(mainComputerID))
+local upMessage = 0
+local downMessage = "Recieved " .. tostring(os.getComputerID() .. "from down block")
 
+    -- wraping peripherals
+
+local digitalController = peripheral.wrap(digitalControllerName)
+local modem = peripheral.wrap("bottem")
+
+-- open rednet to send / recieve messages
+
+rednet.open(modem)
+
+-- set block to Occupied State
 function Occupied()
 
     digitalController.setAspect(digitalList[1], 5)
@@ -26,6 +38,8 @@ function Occupied()
 
 end
 
+
+-- set block to Caution State
 function Warning()
        
     digitalController.setAspect(digitalList[1], 1)
@@ -35,6 +49,7 @@ function Warning()
 
 end
 
+-- set block to Clear State
 function Clear()
         
     digitalController.setAspect(digitalList[1], 1)
@@ -44,26 +59,81 @@ function Clear()
 
 end
 
-function updateMain(status)
-
-    rednet.send(status)
+-- up dates Main Computer and other block computers
+function sendMessage(Device, Status, Recieved, computers)
     
-end
-
-while true do
-    
-    trainPass = redstone.getInput("top")
-
-    if trainPass > 0 then
+    for i in ipairs(Recieved) do
         
-        Occupied()
-        blockStatus = 0
-        mainToChange = 1
+        if Recieved[i] == 0 then
+            
+            if i == 2 then
+                
+                message = "recieved " .. tostring(Device)
 
-        return blockStatus, mainToChange
+                return message
+
+            else
+
+                message = Status
+
+                return message
+
+            end
+
+            rednet.send(computers[i], message)
+
+        end
+
+        Recieved[i] = 0
 
     end
+end
+
+function recievedMessage()
+
+    local computerID, Message = rednet.receive()
 
 
 
+end    
+
+
+-- main loop of the signal
+while true do
+    
+    trainPass = redstone.getInput("top")                        -- Redstone from Detector
+
+    if trainPass > 0 and reset == 0 then                        -- check if train has entered controlled block
+        
+        blockStatus = 0
+        local reset = 1
+
+        return blockStatus, reset
+
+    elseif trainPass == 0 then                                  -- reset detector
+        
+        reset = 0
+
+    elseif blockStatus == 2 then                                -- allows the clear function to run
+        
+        Clear()
+        local mainToChange = 1
+
+        return mainToChange
+
+    elseif blockStatus == 1 then                                -- allows the caution function to run
+
+        Warning()
+        local mainToChange = 1
+
+        return mainToChange
+    
+    elseif blockStatus == 0 then                                -- allows the occupied function to run
+
+        Occupied()
+        local mainToChange = 1
+
+        return mainToChange
+
+    end
 end
--- Common.lua
+++
@@ -1,7 +0,0 @@
--- 
-
--- Modem Chanels
-
-Signal_Channel = 10 
-Points_Channel = 15
-Monitor_Channel = 5
