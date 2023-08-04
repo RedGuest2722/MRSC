046818f50cc67531ae1e494e20252126097039d0
RedGuest2722 <willbrunker123@gmail.com>
2023-08-04 17:00:10 +0100

    new repeater signal to test deleted old signal script renamed alt_autoSignal to autoSignal started script for junction signals

--- Signals/autoSignal.lua
+++ Signals/autoSignal.lua
@@ -1,148 +1,125 @@
--- auto signal
+-- auto Signal
 
-
--- varibles
-
+-- variables
 digitalList = {"Locking", "Clear", "Caution", "Signal"}
-channelList = {1, 2}
 
-    -- wraping peripherals
-
+-- wraping peripherals
 digitalController = peripheral.wrap("bottom")
 
-modem_main = peripheral.wrap("back")
-modem_down = peripheral.wrap("left")
-modem_up = peripheral.wrap("right")
+modemMain = peripheral.wrap("back")
+modemDown = peripheral.wrap("left")
+modemUp = peripheral.wrap("right")
 
--- set block to Occupied State
-function Occupied()
+-- ports
+modemMain.open(5)
+modemDown.open(2)
+modemUp.open(2)
 
-    digitalController.setAspect(digitalList[2], 1)
-    digitalController.setAspect(digitalList[3], 1)
-    digitalController.setAspect(digitalList[4], 5)
 
-    modem_down.transmit(channelList[2], channelList[2], "Caution")
+-- change screen colours
+function screen(colour)
 
-    downRecieve = 0
-
-    print("Train Wait")
-
-    repeat
-        
-        os.sleep(0.1)
-
-    until redstone.getAnalogInput("top") == 0
-
-    digitalController.setAspect(digitalList[1], 5)
-
-    print("In danger state")
-
-    return downRecieve
+    term.setBackgroundColor(colour)
+    term.clear()
 
 end
 
+function occupied()
 
--- set block to Caution State
-function Warning()
-       
     digitalController.setAspect(digitalList[1], 1)
+    digitalController.setAspect(digitalList[2], 1)
+    digitalController.setAspect(digitalList[3], 5)
+    digitalController.setAspect(digitalList[4], 5)
+    
+    screen(colors.red)
+
+end
+
+function caution()
+   
+    digitalController.setAspect(digitalList[1], 5)
     digitalController.setAspect(digitalList[2], 1)
     digitalController.setAspect(digitalList[3], 5)
     digitalController.setAspect(digitalList[4], 3)
-
-    modem_down.transmit(channelList[2], channelList[2], "Clear")
-
-    downRecieve = 0
-
-    print("In caution state")
-
-    return state
-
-end
-
--- set block to Clear State
-function Clear()
-        
-    digitalController.setAspect(digitalList[1], 1)
-    digitalController.setAspect(digitalList[2], 1)
-    digitalController.setAspect(digitalList[3], 5)
-    digitalController.setAspect(digitalList[4], 1)
-
-    print("In clear state")
-
-end
-
-function trainCheck()
-
-    trainpass = redstone.getAnalogInput("top")
-
-    if trainpass > 0 then
     
-        Occupied()
+    screen(colors.orange)
 
+end
+
+function clear()
+
+    digitalController.setAspect(digitalList[1], 5)
+    digitalController.setAspect(digitalList[2], 5)
+    digitalController.setAspect(digitalList[3], 1)
+    digitalController.setAspect(digitalList[4], 1) 
+    
+    screen(colors.green)  
+
+end
+
+-- this tells the computer what block state to be in
+function updateBlock(stateChange)
+
+    if stateChange == "occupied" then
+        occupied() 
+        modemDown.transmit(2, 500, "caution")
+        state = {"occupied", 1, 1}
+    elseif stateChange == "caution" then
+        caution()
+        modemDown.transmit(2, 500, "clear")
+        state = {"caution", 1, 1}
+    elseif stateChange == "clear" then
+        clear()
+        state = {"clear", 0, 0}
     end
 end
 
-function updateBlock()
-
-   if message == "Caution" then
+-- see if train has occupied the block
+function trainCheck()
     
-        Warning()
+    if redstone.getAnalogInput("top") > 0 then
 
-    elseif message == "Clear" then
+        repeat
+        
+            os.sleep(0.1)
+    
+        until redstone.getAnalogInput("top") == 0
+        
+        updateBlock("occupied")
 
-        Clear()
-
-   end 
+    end
 end
 
 function messageCheck()
 
-    os.startTimer(0.1)
-
-    modem_down.open(channelList[2])
+    os.startTimer(0.1) -- this stops the os.pull() from running indefinitly
     event, side, senderChannel, replyChannel, message, senderDistance = os.pullEvent()
-    modem_down.close(channelList[2])
 
     if event == "modem_message" then
-
-        if senderChannel == channelList[1] then
-            
-            mainRecieve = 1
-
-            message = "noChange"
-
-        elseif senderChannel == channelList[2] then
-
-            downRecieve = 1
-
-            message = "noChange"
-         
-        elseif senderChannel == channelList[2] then
-
-            upSend = 1
-
-        end
-    
-    else
         
-        message = "noChange"
+        if side == "right" then
+            
+            if message == "caution" then
 
-    
+                updateBlock("caution")
+                
+            elseif message == "clear" then
+
+                updateBlock("clear")
+
+            end
+        end
     end
-
 end
 
--- Start up
+-- startup
 
-term.clear()
-state = Occupied()
+state = {"occupied", 1, 1}
+updateBlock("occupied")
 
--- main loop of the signal
 while true do
-  
-messageCheck()
-updateBlock()
-trainCheck()
+
+    trainCheck()
+    messageCheck()
 
 end
